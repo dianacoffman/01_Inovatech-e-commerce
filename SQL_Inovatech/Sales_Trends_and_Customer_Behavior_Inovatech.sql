99eeb28f34013c407dedf3f836fe3e1a0683e485
Diana Coffman <dcoffman0396@gmail.com>
2024-09-11 09:09:51 -0700

    Update Sales_Trends_and_Customer_Behavior_Inovatech.sql

--- SQL_Inovatech/Sales_Trends_and_Customer_Behavior_Inovatech.sql
+++ SQL_Inovatech/Sales_Trends_and_Customer_Behavior_Inovatech.sql
@@ -4,11 +4,23 @@
 --Monthly refund rate:
 select
 os.purchase_date_month,
-round(count(os.refund_date)/count(distinct os.order_id)*100,2) as refund_rate,
+round(count(os.refund_date)/count(distinct os.order_id)*100,2) as refund_rate
 from order_status_cleaned os
 group by os.purchase_date_month
 order by os.purchase_date_month desc
 ;
+--Top month refunds across all years: 2 (2.19) > 5 (1.99) > 1 (1.93)
+
+----Year refund rate:
+select
+os.purchase_date_month,
+os.purchase_date_year,
+round(count(os.refund_date)/count(distinct os.order_id)*100,2) as refund_rate
+from order_status_cleaned os
+group by os.purchase_date_year, os.purchase_date_month
+order by refund_rate desc
+;
+--Top year refunds: No refunds for 2022 / 2020 (months 7 (3.95) > 10 (3.44)) > 2 (3.38) / 2021 (months 2 (3.08) > 1 (2.86)) / 2019 (months 12 (2.3 > 4 (2.21) > 5 (2.17))
 
 --Monthly refund rate in 2020:
 select
@@ -19,6 +31,7 @@ where os.purchase_date_year = 2020
 group by os.purchase_date_month
 order by os.purchase_date_month desc
 ;
+--Top refunds 2020: months 7 (3.95) > 10 (3.44) > 2 (3.38)
 
 --Most Refunded Products (which products had the highest total count of refunds?):
 select
@@ -46,12 +59,13 @@ group by o.product_name
 order by round(count(os.refund_date)/count(distinct os.order_id)*100,2) desc
 limit 3
 ;
---Results: Macbook Air Laptop, ThinkPad Laptop, Apple iPhone.
+--Top products refunded: Macbook Air Laptop (4.17) > ThinkPad Laptop (3.83) > Apple iPhone (3.52)
 
 --Monthly Apple Product Refunds in 2021 (how many refunds were there each month for Apple products in 2021?):
 select
 os.purchase_date_month,
-round(count(os.refund_date)/count(distinct os.order_id)*100,2) as refund_rate,
+count(os.refund_date) as refund_count,
+round(count(os.refund_date)/count(distinct os.order_id)*100,2) as refund_rate
 from order_status_cleaned os
 join orders_cleaned o
 on os.order_id = o.id
@@ -59,12 +73,27 @@ where os.purchase_date_year = 2021 and lower(o.product_name) like '%apple%'
 group by os.purchase_date_month
 order by os.purchase_date_month
 ;
+--Top month refunds: month 2 (33) > month 1 (29) > month 3 (29)
 
 --2. ACCOUNT CREATION METHODS
+--Average order value across different account creation methods
+select
+c.account_creation_method as method,
+avg(o.usd_price) as aov_price
+from orders_cleaned o
+join customers_cleaned c
+on o.customer_id = c.id
+and c.account_creation_method not like "unknown"
+group by c.account_creation_method
+order by avg(o.usd_price) desc
+;
+--Top Average order value across different account creation methods: 2018: tablet - 359.1947), 2019: (tv - 265.8820), 2020: (desktop - 318.3014), 2021 (tv	318.1514), 2022: (desktop - 232.6226)
+--Top amount order value across different account creation methods: (method, avo, year, amount) = desktop 318.3014, 2020, 19967 / desktop	259.5465, 2021, 18773 / desktop	260.3914, 2019, 11266 / desktop	232.6226, 2022, 9436 / desktop 259.2877, 2018, 1636
+
 --Average order value across different account creation methods in the first two months of 2022:
 select
 c.account_creation_method as method,
-avg(o.usd_price) as aov_price,
+avg(o.usd_price) as aov_price
 from orders_cleaned o
 join customers_cleaned c
 on o.customer_id = c.id
@@ -90,7 +119,7 @@ order by count(distinct(o.customer_id)) desc
 ;
 
 --3. MARKETING CHANNELS
---Which marketing channels perform the best in each region? Does the top channel differ across regions?
+--Which marketing channels perform the best in each region?
 select
 c.region,
 c.marketing_channel,
@@ -102,12 +131,15 @@ join customers_cleaned c
 on o.customer_id = c.id
 where c.marketing_channel not like "unknown"
 and c.region is not null
-group by c.region
+group by c.region, c.marketing_channel
 order by c.region desc,
 c.marketing_channel desc
 ;
+--Direct in all region is the marketing channels perform the best related total sales.
+--Top AOV_price: NA (afiliate) = 347.55 > APAC (direct) = 288.95 > EMEA (Afiliate) = 285.95  > LATAM (direct) = 254.97
+--Top num_orders: NA (direct) = 30793 > EMEA (direct) = 18801  > APAC (direct) = 7598 > LATAM (direct) = 3864
 
---Does the top channel differ across regions?
+--Does the top channel differ across regions related to sales?
 WITH regional_sales as
 (SELECT
    c.region,
@@ -136,6 +168,7 @@ JOIN (
 ) AS top_channels
 ON rs.region = top_channels.region
 AND rs.total_sales = top_channels.max_sales;
+--The top marketing channel differ across regios about total sales: NA = direct, total sales: 8453401.99 > EMEA = direct, 5138337.47 > APAC = direct, 2195454.59 > LATAM = direct, 985201.21
 
 --4. CUSTOMER ORDERS
 --For customers who made more than 4 orders across all years, what was the order ID, product, and purchase date of their most recent order? 
